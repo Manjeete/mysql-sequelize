cfc536300f6e80d9b722758702d47b540340f923
manjeete <manjeetkr2017@gmail.com>
2022-01-13 20:29:41 +0530

    database connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const PORT = 8080;
+require("./models");
 
 app.get("/",(req,res) =>{
     res.send("Working...")
--- models/index.js
+++ models/index.js
@@ -1,1 +1,25 @@
 const Sequelize = require('sequelize')
+const dbConfig = require("../config/dbConfig");
+
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,{
+        host:dbConfig.HOST,
+        dialect:'mysql',
+        pool:{
+            max:5,
+            min:0,
+            idle:10000
+        }
+    }
+
+);
+
+sequelize.authenticate()
+.then(() =>{
+    console.log("Database connected...")
+})
+.catch(err =>{
+    console.log("Error:"+err)
+})
